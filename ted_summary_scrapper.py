0ede7b72a40edc3a277cec2f8c5242236a186f9d
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2021-11-26 18:52:39 -0500

    New: tedsummaries edge cases handling

--- ted_summary_scrapper.py
+++ ted_summary_scrapper.py
@@ -4,10 +4,10 @@ All relevant information are in the static website
 """
 
 import re
-
+import json
 import requests
 
-import bs4
+# import bs4
 from bs4 import BeautifulSoup
 from icecream import ic
 
@@ -19,8 +19,7 @@ def get_soup(url):
 
 
 def get_words(s: str) -> list[str]:
-    # Remove empty strings
-    return list(filter(bool, re.split(r'\W+', s)))
+    return list(filter(bool, re.split(r'\W+', s)))  # Remove empty strings
 
 
 class TedSummaryScrapper:
@@ -30,6 +29,9 @@ class TedSummaryScrapper:
         self.link_divisions = self._link_divisions_by_month()
         # ic(self.link_divisions)
         self.links = self._sum_links()
+        self.links = {k: list(  # Not a summary
+            filter(lambda link: link != 'https://tedsummaries.com/2014/08/03/tedsummaries-questions-to-you/', v)
+        ) for k, v in self.links.items()}
         # ic(self.links, len(flatten(self.links.values())))
         self.sums = self._sums()
         ic(self.sums)
@@ -59,30 +61,8 @@ class TedSummaryScrapper:
         """
         # for month, links in self.links.items():
         def link2dict(link):
-            # if month == (2015, 4):
-            #     ic(link)
-            def soup2summary(s_):
-                """
-                :param s_: `BeautifulSoup` object for a `tedsummaries` page
-                """
-                entries = s_.find_all('div', class_='entry-content')
-                assert len(entries) == 1
-                entry = entries[0]
-
-                def elm2str(elm):
-                    if elm.name == 'p':
-                        return elm.text
-                    else:  # `ol` or 'ul'
-                        ic('here')
-                        return '\n'.join(f'{idx+1}. {li.text}' for idx, li in enumerate(elm.find_all('li')))
-
-                elms = entry.find_all(['p', 'ol', 'ul'])
-                idx_strt = [p.__str__() for p in elms].index('<p><strong>Summary</strong></p>')
-                idx_end = [p.__str__() for p in elms].index('<p><strong>My Thoughts</strong></p>')
-                elms = elms[idx_strt+1:idx_end]
-                return dict(
-                    summary='\n'.join(elm2str(elm) for elm in elms)
-                )
+            if not hasattr(link2dict, 'count'):
+                link2dict.count = 0
 
             def soup2meta(s_):
                 titles = s_.find_all('h1', class_='entry-title')
@@ -93,6 +73,51 @@ class TedSummaryScrapper:
                 return dict(
                     speaker=m.group('speaker').lower(),
                     title=m.group('title').lower()
+                )
+
+            def soup2summary(s_):
+                """
+                :param s_: `BeautifulSoup` object for a `tedsummaries` page
+                """
+                def elm2str(elm):
+                    if elm.name == 'p':
+                        return elm.text
+                    else:  # `ol` or 'ul'
+                        return '\n'.join(f'{idx+1}. {li.text}' for idx, li in enumerate(elm.find_all('li')))
+
+                def txt2dom(s, lh=False):
+                    """
+                    Heuristic for the summary element
+                    """
+                    return (
+                        (lh and '<p><span style="line-height:1.5em;"><strong>Summary</strong></span></p>') or
+                        f'<p><strong>{s}</strong></p>'
+                    )
+
+                entries = s_.find_all('div', class_='entry-content')
+                assert len(entries) == 1
+                # The summary may contain `blockquote`s, ignored
+                elms = entries[0].find_all(['p', 'ol', 'ul'])
+
+                e_strs = [p.__str__() for p in elms]
+                ic(e_strs)
+
+                s_tho = txt2dom('My Thoughts')
+                s_sum = txt2dom('Summary')
+                s_sum_ = txt2dom('Summary:')
+                s_sum__ = txt2dom('Summary', lh=True)
+                idx_strt = (
+                    (s_sum in e_strs and e_strs.index(s_sum)) or
+                    (s_sum_ in e_strs and e_strs.index(s_sum_)) or
+                    (s_sum__ in e_strs and e_strs.index(s_sum__))
+                )
+                ic(idx_strt)
+                assert type(idx_strt) is int
+
+                idx_end = (s_tho in e_strs and e_strs.index(s_tho)) or None
+                elms = elms[idx_strt+1:idx_end]
+                return dict(
+                    summary='\n'.join(elm2str(elm) for elm in elms)
                 )
 
             def soup2transcript(s_: BeautifulSoup):
@@ -109,24 +134,43 @@ class TedSummaryScrapper:
                 return dict(
                     transcript='\n'.join(cluster2txt(cls) for cls in trans_clusters)
                 )
+            print(f'Scrapping ted talk {link2dict.count+1}: [{link}]')
             soup = get_soup(link)
-            d = soup2summary(soup) | soup2meta(soup)
+            d = soup2meta(soup) | soup2summary(soup)
+            # if d['speaker'] == 'JANE MCGONIGAL'.lower():
+            #     ic(d)
+            #     exit(1)
             words = get_words(d['speaker']) + get_words(d['title'])
             # TED talk url eg: charmian_gooch_meet_global_corruption_s_hidden_players
             url_ted = f'https://www.ted.com/talks/{"_".join(words)}/transcript'
             # ic(url_ted)
-            return d | soup2transcript(get_soup(url_ted))
+            d |= soup2transcript(get_soup(url_ted))
+            link2dict.count += 1
+            print(f'Scrapping ted talk {link2dict.count}: [{d["title"]}] by [{d["speaker"]}] completed')
+            return d
 
         links = flatten([
             (date, link) for link in links
         ] for date, links in self.links.items())
         for date, link in links:
-            if date == (2015, 4):
-                ic(link2dict(link))
-                exit(1)
-        # return [dict(year=year, month=month) | link2dict(link) for (year, month), link in links]
+            if date == (2014, 3):
+                link2dict(link)
+                # exit(1)
+        return [dict(year=year, month=month) | link2dict(link) for (year, month), link in links]
+
+    def export(self, fp):
+        """
+        Export scrapped summaries to JSON
+
+        :param fp: File path without extension
+        """
+        fnm = f'{fp}.json'
+        open(fnm, 'a').close()  # Create file in OS
+        with open(fnm, 'w') as f:
+            json.dump(self.sums, f, indent=4)
 
 
 if __name__ == '__main__':
     tss = TedSummaryScrapper()
+    tss.export('ted-summaries')
     # ic(tss.month_links)
--- test_lang.py
+++ test_lang.py
@@ -2,26 +2,30 @@ from icecream import ic
 
 
 if __name__ == '__main__':
-    import re
+    # import re
+    #
+    # m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist")
+    # ic(m.groups())
+    #
+    # s = 'https://tedsummaries.com/2014/04/'
+    # ic(s)
+    # m = re.match(r".*?(\d+)", '2014/04/')
+    # ic(m.groups())
+    # m = re.match(r'.*?(?P<year>\d{4})[/](?P<month>\d{2})[/]$', 'https://tedsummaries.com/2014/04/')
+    # ic(m and m.groups())
+    # ic(m.group('year'))
+    # ic(m.group('month'))
+    #
+    # # ic(re.match(r'(\d{4})', s))
+    # # ic(re.match(r'^/\d{4}/\d{2}/$', s))
+    #
+    # ic(re.split(r'\W+', 'Words, words, words.'))
+    #
+    # myString = "I want to Remove all white \t spaces, new lines \n and tabs \t"
+    # ic(re.sub(r"[\n\t\s]*", "", myString))
+    # ic(re.sub(r"[\n\t]+", "", myString))
 
-    m = re.match(r"(\w+) (\w+)", "Isaac Newton, physicist")
-    ic(m.groups())
+    arr = [1, 2, 3]
+    ic(arr.index(4))
 
-    s = 'https://tedsummaries.com/2014/04/'
-    ic(s)
-    m = re.match(r".*?(\d+)", '2014/04/')
-    ic(m.groups())
-    m = re.match(r'.*?(?P<year>\d{4})[/](?P<month>\d{2})[/]$', 'https://tedsummaries.com/2014/04/')
-    ic(m and m.groups())
-    ic(m.group('year'))
-    ic(m.group('month'))
-
-    # ic(re.match(r'(\d{4})', s))
-    # ic(re.match(r'^/\d{4}/\d{2}/$', s))
-
-    ic(re.split(r'\W+', 'Words, words, words.'))
-
-    myString = "I want to Remove all white \t spaces, new lines \n and tabs \t"
-    ic(re.sub(r"[\n\t\s]*", "", myString))
-    ic(re.sub(r"[\n\t]+", "", myString))
 
