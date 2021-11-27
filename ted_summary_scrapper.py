5045302ce38c4cbc785c18cc30235e911b9e3d71
Stefan Heng <43276957+SpongeBobBang@users.noreply.github.com>
2021-11-26 21:14:42 -0500

    Update: loop through all talks

--- ted_summary_scrapper.py
+++ ted_summary_scrapper.py
@@ -4,6 +4,8 @@ All relevant information are in the static website
 """
 
 import re
+from typing import Union
+from warnings import warn
 import json
 import requests
 
@@ -29,8 +31,14 @@ class TedSummaryScrapper:
         self.link_divisions = self._link_divisions_by_month()
         # ic(self.link_divisions)
         self.links = self._sum_links()
-        self.links = {k: list(  # Not a summary
-            filter(lambda link: link != 'https://tedsummaries.com/2014/08/03/tedsummaries-questions-to-you/', v)
+        self.links = {k: list(
+            filter(lambda link: link not in [
+                'https://tedsummaries.com/2014/08/03/tedsummaries-questions-to-you/',  # Not a summary
+                # Not found on ted.com
+                'https://tedsummaries.com/2013/12/27/the-paradoxes-of-power-in-australia-geoff-aigner/',
+                'https://tedsummaries.com/2013/10/06/biochemical-degradation-of-plastics-phthalates/',
+                'https://tedsummaries.com/2013/10/05/welcome/'
+            ], v)
         ) for k, v in self.links.items()}
         # ic(self.links, len(flatten(self.links.values())))
         self.sums = self._sums()
@@ -67,12 +75,35 @@ class TedSummaryScrapper:
             def soup2meta(s_):
                 titles = s_.find_all('h1', class_='entry-title')
                 assert len(titles) == 1
-                title = titles[0]
+                txt = titles[0].text
+                txt = txt.replace('\xa0', ' ')  # A different encoding
 
-                m = re.match(r'^(?P<speaker>.*?): (?P<title>.*?)$', title.text)
-                return dict(
-                    speaker=m.group('speaker').lower(),
-                    title=m.group('title').lower()
+                m = re.match(r'^(?P<speaker>.*?): (?P<title>.*?)$', txt)
+                ic(txt)
+                d_map = {  # Some earlier entries that doesn't follow the heuristic
+                    'Jill Bolte Taylor’s stroke of insight': dict(
+                        speaker='Jill Bolte Taylor', title='My stroke of insight'
+                    ),
+                    'A realistic vision for World Peace': dict(
+                        speaker='Jody Williams', title='A realistic vision for World Peace'
+                    ),
+                    'Treating violence as a contagious disease': dict(
+                        speaker='Gary Slutkin', title='Treating violence as a contagious disease'
+                    ),
+                    'Online Filter Bubbles': dict(
+                        speaker='Eli Pariser', title='Online Filter Bubbles'
+                    ),
+                    'Can technology solve big problems?': dict(
+                        speaker='Jason Pontin', title='Can technology solve big problems?'
+                    ),
+                    'How to learn anything in 20 hours': dict(
+                        speaker='Josh Kaufman', title='How to learn anything in 20 hours'
+                    )
+
+                }
+                return (  # Noise in the blog
+                    (txt in d_map and d_map[txt]) or
+                    dict(speaker=m.group('speaker'), title=m.group('title'))
                 )
 
             def soup2summary(s_):
@@ -85,36 +116,56 @@ class TedSummaryScrapper:
                     else:  # `ol` or 'ul'
                         return '\n'.join(f'{idx+1}. {li.text}' for idx, li in enumerate(elm.find_all('li')))
 
-                def txt2dom(s, lh=False):
-                    """
-                    Heuristic for the summary element
-                    """
-                    return (
-                        (lh and '<p><span style="line-height:1.5em;"><strong>Summary</strong></span></p>') or
-                        f'<p><strong>{s}</strong></p>'
-                    )
+                # def txt2dom(s, lh=False):
+                #     """
+                #     Heuristic for the summary element
+                #     """
+                #     return (
+                #         (lh and f'<p><span style="line-height:1.5em;"><strong>{s}</strong></span></p>') or
+                #         f'<p><strong>{s}</strong></p>'
+                #     )
 
                 entries = s_.find_all('div', class_='entry-content')
                 assert len(entries) == 1
                 # The summary may contain `blockquote`s, ignored
                 elms = entries[0].find_all(['p', 'ol', 'ul'])
 
-                e_strs = [p.__str__() for p in elms]
-                ic(e_strs)
+                # e_strs = [p.__str__() for p in elms]
+                # ic(e_strs)
 
-                s_tho = txt2dom('My Thoughts')
-                s_sum = txt2dom('Summary')
-                s_sum_ = txt2dom('Summary:')
-                s_sum__ = txt2dom('Summary', lh=True)
-                idx_strt = (
-                    (s_sum in e_strs and e_strs.index(s_sum)) or
-                    (s_sum_ in e_strs and e_strs.index(s_sum_)) or
-                    (s_sum__ in e_strs and e_strs.index(s_sum__))
-                )
-                ic(idx_strt)
+                def contains_text(elm, txt: Union[str, list[str]]):
+                    texts_elm = [s.text for s in elm.find_all('strong')]
+                    if not isinstance(txt, list):
+                        txt = [txt]
+                    # 'Summary' in texts_elm or 'Summary:' in texts_elm
+                    # ic(texts_elm, txt)
+                    return any(t in texts_elm for t in txt)
+                # Should always be one
+                elm_sums = list(filter(lambda e: contains_text(e, ['Summary', 'Summary:']), elms))
+                elm_2nd = list(filter(lambda e: contains_text(e, ['My Thoughts', 'Critique']), elms))
+                # ic(elm_sums)
+                assert len(elm_sums) == 1
+                # elm_sum =
+
+                # s_tho = txt2dom('My Thoughts')
+                # s_sum = txt2dom('Summary')
+                # s_sum_ = txt2dom('Summary:')
+                # s_sum__ = txt2dom('Summary', lh=True)
+                # idx_strt = (
+                #     (s_sum in e_strs and e_strs.index(s_sum)) or
+                #     (s_sum_ in e_strs and e_strs.index(s_sum_)) or
+                #     (s_sum__ in e_strs and e_strs.index(s_sum__))
+                # )
+                idx_strt = elms.index(elm_sums[0])
                 assert type(idx_strt) is int
+                if elm_2nd:
+                    idx_end = elms.index(elm_2nd[0])
+                else:
+                    idx_end = None
+                    warn(f'Title after summary not found for [{link}]')
+                # ic(idx_strt)
 
-                idx_end = (s_tho in e_strs and e_strs.index(s_tho)) or None
+                # idx_end = (s_tho in e_strs and e_strs.index(s_tho)) or None
                 elms = elms[idx_strt+1:idx_end]
                 return dict(
                     summary='\n'.join(elm2str(elm) for elm in elms)
@@ -134,29 +185,29 @@ class TedSummaryScrapper:
                 return dict(
                     transcript='\n'.join(cluster2txt(cls) for cls in trans_clusters)
                 )
-            print(f'Scrapping ted talk {link2dict.count+1}: [{link}]')
+            print(f'Scrapping TED talk {link2dict.count+1}: [{link}]')
             soup = get_soup(link)
             d = soup2meta(soup) | soup2summary(soup)
             # if d['speaker'] == 'JANE MCGONIGAL'.lower():
             #     ic(d)
             #     exit(1)
-            words = get_words(d['speaker']) + get_words(d['title'])
+            words = get_words(d['speaker'].lower()) + get_words(d['title'].lower())
             # TED talk url eg: charmian_gooch_meet_global_corruption_s_hidden_players
             url_ted = f'https://www.ted.com/talks/{"_".join(words)}/transcript'
             # ic(url_ted)
             d |= soup2transcript(get_soup(url_ted))
             link2dict.count += 1
-            print(f'Scrapping ted talk {link2dict.count}: [{d["title"]}] by [{d["speaker"]}] completed')
+            print(f'Completed scrapping TED talk {link2dict.count}: [{d["title"]}] by [{d["speaker"]}]')
             return d
 
         links = flatten([
             (date, link) for link in links
         ] for date, links in self.links.items())
         for date, link in links:
-            if date == (2014, 3):
+            if date == (2013, 10):
                 link2dict(link)
                 # exit(1)
-        return [dict(year=year, month=month) | link2dict(link) for (year, month), link in links]
+        # return [dict(year=year, month=month) | link2dict(link) for (year, month), link in links]
 
     def export(self, fp):
         """
